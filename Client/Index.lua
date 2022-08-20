f9d03e0f41c7e0fb7ca7607017d86ab6772a32ac
dkfn <dkfn@debian-BULLSEYE-live-builder-AMD64>
2022-08-20 17:21:35 +0200

    WIP

--- Client/Index.lua
+++ Client/Index.lua
@@ -1,37 +1,71 @@
 local EDITOR_GUI_DEV = true;
 local editorWindowOpen = false;
+local crtlMode = false
 
 local editorWebUi;
 
 local pkgData;
 
 Input.Register("OpenEditor", "F8")
+Input.Register("CrtlEditor", "LeftControl")
+Input.Register("CrtlEditor_ReloadAllPackages", "R")
+Input.Register("CrtlEditor_SaveCurrentFile", "S")
 
+local handleAsKFileContents = function(data)
+    local parsedData = JSON.parse(data)
+    Package.Log("Received asked content from parsed content"..NanosUtils.Dump(parsedData))
+    Events.CallRemote("NIDE:JS_ASK_FILE_CONTENTS", parsedData.packageName, parsedData.filePath)
+end
+
+-- Editor init
 Input.Bind("OpenEditor", InputEvent.Pressed, function()
-    if (editorWindowOpen) then
-        editorWebUi:Destroy()
+    if (editorWebUi == nil) then
+        editorWebUi = WebUI(
+            "NanosIDE",
+            EDITOR_GUI_DEV and "http://localhost:1234/" or "file://gui/index.html",
+            true
+        )
+
+        editorWebUi:Subscribe("NIDE:CLIENT_ASK_FILE_CONTENTS", handleAsKFileContents)
+        editorWebUi:Subscribe("Ready", function()
+            Events.CallRemote("NIDE:CLIENT_INIT_IDE")
+            Client.SetMouseEnabled(true)
+            Client.SetInputEnabled(true)
+        end)
+        editorWebUi:SetFocus()
     else
-            editorWebUi = WebUI(
-                    "NanosIDE",
-                    EDITOR_GUI_DEV and "http://localhost:1234/" or "file://gui/index.html",
-                    true
-            )
-
-            editorWebUi:Subscribe("NIDE:CLIENT_ASK_FILE_CONTENTS", function(data)
-                local parsedData = JSON.parse(data)
-                Package.Log("Received asked content from parsed content"..NanosUtils.Dump(parsedData))
-                Events.CallRemote("NIDE:JS_ASK_FILE_CONTENTS", parsedData.packageName, parsedData.filePath)
-            end)
+        if (editorWebUi:IsValid()) then
+            -- editorWebUi:Unsuscribe("NIDE:CLIENT_ASK_FILE_CONTENTS", handleAsKFileContents)
+            editorWebUi:RemoveFocus()
+            Timer.SetTimeout(function()
+                editorWebUi:Destroy()
+                editorWebUi = nil
+            end, 100)
+            Client.SetMouseEnabled(false)
+            Client.SetInputEnabled(true)
         end
-    end)
-
-Events.Subscribe("NIDE:CLIENT_SEND_PKG_INFOS", function(_pkgData)
-    -- Package.Log("Received frontend data for pkg : "..NanosUtils.Dump(_pkgData))
-    pkgData = JSON.parse(_pkgData)
-    editorWebUi:CallEvent("NIDE:JS_SEND_PKG_INFOS", _pkgData)
+    end
 end)
 
+Events.Subscribe("NIDE:CLIENT_SEND_PKG_INFOS", function(_pkgData)
+    Package.Log("Received frontend data for pkg :< "..NanosUtils.Dump(_pkgData))
+    if editorWebUi then
+        pkgData = JSON.parse(_pkgData)
+        editorWebUi:CallEvent("NIDE:JS_SEND_PKG_INFOS", _pkgData)
+    end
+end)
+
+-- File browsing
 Events.Subscribe("NIDE:CLIENT_SEND_FILE_CONTENTS", function(fileContent)
     editorWebUi:CallEvent("NIDE:JS_SEND_SEND_FILE_CONTENTS", fileContent)
+end)
 
+-- Control actions
+Input.Bind("ControlMode", Input.Pressed, function()
+    crtlMode = true
+end)
+
+
+Input.Bind("ControlMode", Input.Released, function()
+    crtlMode = false
 end)
--- Server/Index.lua
+++ Server/Index.lua
@@ -7,24 +7,33 @@ Events.Subscribe("NIDE:SEND_PKG_INFOS", function(pkgName, infos)
     pkgData[pkgName] = infos
 end)
 
-Events.Subscribe("NIDE:GET_FILE_CONTENTS", function(packageName, packageFiles)
-    local stringContent = loadFileContent(packageName, packageFiles)
-end)
-
 local function loadFileContent(packageName, packageFiles)
     local fileHandle = io.open( "Packages/"..packageName..packageFiles, "r" )
     local sContents = fileHandle:read( "*all" )
     Package.Log("File content : "..sContents)
+    return sContents
 end
+
+Events.Subscribe("NIDE:GET_FILE_CONTENTS", function(packageName, packageFiles)
+    local stringContent = loadFileContent(packageName, packageFiles)
+end)
+
+
+Events.Subscribe("NIDE:CLIENT_INIT_IDE", function(player)
+    Package.Log("Current pkg data : "..NanosUtils.Dump(pkgData))
+    Package.Log("Client has initied the IDE"..NanosUtils.Dump(player))
+    Events.CallRemote("NIDE:CLIENT_SEND_PKG_INFOS", player, JSON.stringify(pkgData))
+end)
 
 
 Package.Subscribe("Load", function()
     Events.Call("NIDE:GET_PKG_FILES", nil)
+    Events.Call("NIDE:ON_ASK_PKG_INFOS")
     Timer.SetInterval(function()
         Events.Call("NIDE:ON_ASK_PKG_INFOS")
-    end, 1000)
+    end, 10000)
 
-    Timer.SetInterval(function()
-        Events.BroadcastRemote("NIDE:CLIENT_SEND_PKG_INFOS", JSON.stringify(pkgData))
-    end, 1000)
+    -- Timer.SetInterval(function()
+    --     Events.BroadcastRemote("NIDE:CLIENT_SEND_PKG_INFOS", JSON.stringify(pkgData))
+    -- end, 1000)
 end)
